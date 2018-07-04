c43362f55b143acd07617a935e0f2467a63695bf
XXWolfBane <XXWolfBane@Gmail.com>
2018-07-04 03:39:43 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ async def on_ready():
 async def greet(ctx):
     await ctx.send(":smiley: :wave: Hello, there!")
 @bot.command()
-async def whoami(ctx)
+async def creds(ctx):
   embed = discord.Embed(title="Python Bot", description="This is who made me!", color=0xeee657)
      embed.add_field(name="Code Author", "XXWolfBane#5559")
      embed.add_field(name="Guild owner", owner_id)
