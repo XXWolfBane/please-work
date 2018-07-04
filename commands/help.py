fa2e275bf9964608652524bdd6d0d20d78508dec
XXWolfBane <XXWolfBane@Gmail.com>
2018-07-04 03:41:52 -0400

    Delete help.py

--- commands/help.py
+++
@@ -1,6 +0,0 @@
-@bot.command()
-async def info(ctx):
-    embed = discord.Embed(title="Python Bot", description="I'm a new bot you expect me to have commands?", color=0xeee657)
-     embed.add_field(name="If this works.")
-     
-    await ctx.send(embed=embed)
